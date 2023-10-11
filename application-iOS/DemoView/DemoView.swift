121b1aca917e5dfc1fdabd06802e93a671f3b2ed
Guilherme Freire <gfreire@MB.local>
2023-10-11 17:52:39 -0300

    Initial commit

---
+++ application-iOS/DemoView/DemoView.swift
@@ -0,0 +1,18 @@
+//
+//  DemoView.swift
+//  application-iOS
+//
+//  Created by Guilherme Freire on 11/10/23.
+//
+
+import UIKit
+
+class DemoView: UIView {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
---
+++ application-iOSUITests/application_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  application_iOSUITestsLaunchTests.swift
+//  application-iOSUITests
+//
+//  Created by Guilherme Freire on 11/10/23.
+//
+
+import XCTest
+
+final class application_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
